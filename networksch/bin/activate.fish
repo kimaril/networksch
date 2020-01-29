f1bd851d0401343edada5998103a19f98bf77cd0
Dasha Kim <daria.kim.ar.il@gmail.com>
2020-01-29 19:03:01 +0300

    Homeworks 1 & 2

--- networksch/bin/activate.fish
+++ networksch/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kim/pet-projects/networksch/networksch"
+set -gx VIRTUAL_ENV "/home/kim/juche/pet-projects/networksch/networksch"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(networksch) "
+        if test -n "(networksch) "            
             printf "%s%s" "(networksch) " (set_color normal)
         else
             # ...Otherwise, prepend env
